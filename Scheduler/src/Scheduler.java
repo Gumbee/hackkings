0c7a7cec06c326700c74e377a5720e498056c368
Alessandro Bonardi <ab9515@ic.ac.uk>
2016-11-26 23:52:08 +0000

    Added sort method

--- Scheduler/src/Scheduler.java
+++ Scheduler/src/Scheduler.java
@@ -1,5 +1,8 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by Alessandro on 26/11/2016.
@@ -14,14 +17,24 @@ public class Scheduler {
     }
 
     private static void schedule(Calendar calendar, List<Task> tasks) {
-        
+        tasks = sortTasksByPriorityThenByHours(tasks);
+        tasks.forEach(t -> System.out.println(t.getPriority() + " " + t.getHours()));
     }
 
+
+
     private static void initialiseTasks(List<Task> tasks) {
-        tasks.add(new Task("Maths", 12, 5, 8, 13));
+        tasks.add(new Task("Maths", 8, 5, 8, 13));
         tasks.add(new Task("Programming", 20, 7, 3, 12));
         tasks.add(new Task("Compilers", 50, 10, 1, 30));
+        tasks.add(new Task("Stats", 10, 5, 6, 10));
         tasks.add(new Task("Software Design", 4, 2, 19, 23));
+    }
+
+    private static List<Task> sortTasksByPriorityThenByHours(List<Task> tasks) {
+        tasks = tasks.stream().sorted(Comparator.comparing(Task::getPriority).thenComparing(Task::getHours)).collect(Collectors.toList());
+        Collections.reverse(tasks);
+        return tasks;
     }
 
 }
