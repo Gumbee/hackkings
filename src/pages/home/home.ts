b8425de7cb7421f29ff195526802d2ef9314a275
Alessandro Bonardi <ab9515@ic.ac.uk>
2016-11-27 11:45:55 +0000

    Added write directory function

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -4,14 +4,22 @@ import { NavController } from 'ionic-angular';
 import { CalendarPage } from '../calendar/calendar';
 import { PulsePage } from '../pulse/pulse';
 
+import {File} from 'ionic-native';
+import {writeFile} from "fs";
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-	constructor(public navCtrl: NavController) {
 
+
+  constructor(public navCtrl: NavController) {
+    var cordova: any;
+    const fs:string = cordova.file.dataDirectory;
+    this.createDirectory("\\");
+    //this.createDir("\\", "NEWFOLDER", false);
 	}
 
 	openPage(page: string) {
@@ -25,8 +33,28 @@ export class HomePage {
 					break;
 				default:
 					break;
-			}	
+			}
 		}, 150);
 	}
+
+  createFile(dirEntry, fileName) {
+  // Creates a new file or returns the file if it already exists.
+  dirEntry.getFile(fileName, {create: true, exclusive: false}, function(fileEntry) {
+
+    writeFile(fileEntry, null);
+
+  }, null);
+
+}
+
+  createDirectory(rootDirEntry) {
+  rootDirEntry.getDirectory('NewDirInRoot', { create: true }, function (dirEntry) {
+    dirEntry.getDirectory('images', { create: true }, function (subDirEntry) {
+
+      this.createFile(subDirEntry, "fileInNewSubDir.txt");
+
+    }, null);
+  }, null);
+}
 
 }
